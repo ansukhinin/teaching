ed8e288a6cdf9a745e6e27085a0ac3d49e57bc67
ansukhinin <an.sukhinin@gmail.com>
2021-07-27 12:24:38 +0500

    добавил код для selfedu с git

--- flask/flask_app/selfedu/main.py
+++ flask/flask_app/selfedu/main.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    print( url_for('index') )
+    return render_template('index.html', title = "Главная")
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    print( url_for('about') )
+    return render_template('about.html', title = "О сайте")
 
 if  __name__=="__main__":
     app.run(debug=True)    
